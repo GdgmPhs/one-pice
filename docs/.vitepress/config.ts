e8cc81101d2369fa31a5b36e8c87b82622e78185
‘aAShuo <1264160390@qq.com>
2023-03-30 22:38:38 +0800

    放开

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,71 +12,71 @@ export default defineConfig({
     nav: [],
     sidebar: [
       // 待学习
-      // {
-      //   text: "模块化",
-      //   collapsible: true,
-      //   items: [
-      //     { text: "理解模块化", link: "/module/modular-specification" },
-      //     { text: "模块化与组件化", link: "/module/modular-componentization" },
-      //   ],
-      // },
-      // {
-      //   text: "前端工程化",
-      //   collapsible: true,
-      //   items: [
-      //     // { text: "vite", link: "/engineering/engineering-vite" },
-      //     {
-      //       text: "vuecli5+vue2+ts",
-      //       link: "/engineering/engineering-vuecli5-vue2-ts",
-      //     },
-      //     {
-      //       text: "vite+vue3+ts",
-      //       link: "/engineering/engineering-vite-vue3-ts",
-      //     },
-      //     {
-      //       text: "vite+react18+ts",
-      //       link: "/engineering/engineering-vite-react18-ts",
-      //     },
-      //   ],
-      // },
-      // {
-      //   text: "Vue",
-      //   collapsible: true,
-      //   items: [
-      //     { text: "vue2.x双向绑定原理", link: "/vue/vue-vue2-principle" },
-      //     { text: "vue3.x双向绑定原理", link: "/vue/vue-vue3-principle" },
-      //     { text: "样式穿透", link: "/vue/vue-deep" },
-      //   ],
-      // },
-      // {
-      //   text: "Http",
-      //   collapsible: true,
-      //   items: [
-      //     { text: "jsonp", link: "/http/http-jsonp" },
-      //     { text: "Http缓存", link: "/http/http-cache" },
-      //   ],
-      // },
-      // {
-      //   text: "文件",
-      //   collapsible: true,
-      //   items: [
-      //     { text: "文件操作", link: "/file/file-operation" },
-      //     { text: "文件下载", link: "/file/file-download" },
-      //   ],
-      // },
-      // {
-      //   text: "原型",
-      //   collapsible: true,
-      //   items: [
-      //     { text: "原型与原型链", link: "/prototype/prototype-prototypeChain" },
-      //     { text: "继承", link: "/prototype/prototype-extend" },
-      //   ],
-      // },
-      // {
-      //   text: "服务器",
-      //   collapsible: true,
-      //   items: [{ text: "部署生产环境", link: "/server/server-install" }],
-      // },
+      {
+        text: "模块化",
+        collapsible: true,
+        items: [
+          { text: "理解模块化", link: "/module/modular-specification" },
+          { text: "模块化与组件化", link: "/module/modular-componentization" },
+        ],
+      },
+      {
+        text: "前端工程化",
+        collapsible: true,
+        items: [
+          // { text: "vite", link: "/engineering/engineering-vite" },
+          {
+            text: "vuecli5+vue2+ts",
+            link: "/engineering/engineering-vuecli5-vue2-ts",
+          },
+          {
+            text: "vite+vue3+ts",
+            link: "/engineering/engineering-vite-vue3-ts",
+          },
+          {
+            text: "vite+react18+ts",
+            link: "/engineering/engineering-vite-react18-ts",
+          },
+        ],
+      },
+      {
+        text: "Vue",
+        collapsible: true,
+        items: [
+          { text: "vue2.x双向绑定原理", link: "/vue/vue-vue2-principle" },
+          { text: "vue3.x双向绑定原理", link: "/vue/vue-vue3-principle" },
+          { text: "样式穿透", link: "/vue/vue-deep" },
+        ],
+      },
+      {
+        text: "Http",
+        collapsible: true,
+        items: [
+          { text: "jsonp", link: "/http/http-jsonp" },
+          { text: "Http缓存", link: "/http/http-cache" },
+        ],
+      },
+      {
+        text: "文件",
+        collapsible: true,
+        items: [
+          { text: "文件操作", link: "/file/file-operation" },
+          { text: "文件下载", link: "/file/file-download" },
+        ],
+      },
+      {
+        text: "原型",
+        collapsible: true,
+        items: [
+          { text: "原型与原型链", link: "/prototype/prototype-prototypeChain" },
+          { text: "继承", link: "/prototype/prototype-extend" },
+        ],
+      },
+      {
+        text: "服务器",
+        collapsible: true,
+        items: [{ text: "部署生产环境", link: "/server/server-install" }],
+      },
       {
         text: "其他",
         collapsible: true,
